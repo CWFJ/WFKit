5beec487629a03aa47e8f474072daab5810ac3cd
Jason <345609097@qq.com>
2016-05-06 17:39:12 +0800

    增加WFScrollToTopTool工具，在屏幕有多个scrollView的情况下，解决点击状态栏返回顶部的问题

---
+++ WFKitExample/WFKitExample/Class/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WFKitExample
+//
+//  Created by Jason on 16/5/6.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WFKitExample/WFKitExample/Class/WFKit/WFScrollToTopTool/WFScrollToTopTool.h
@@ -0,0 +1,23 @@
+//
+//  WFScrollToTopTool.h
+//  WFKit
+//
+//  Created by Jason on 16/5/6.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WFScrollToTopTool : NSObject
+#pragma mark ------<启动服务>
+/**
+ *  启动服务
+ */
++ (void)start;
+
+#pragma mark ------<停止服务>
+/**
+ *  停止服务
+ */
++ (void)stop;
+@end
